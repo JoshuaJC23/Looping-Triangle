1a9bd655edfd380c7627143515edebe7297d3cdc
Ball Man <joshua.christensen13@gmail.com>
2022-01-14 12:32:23 -0700

    using the empty string variable is to hold the result of each iteration.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // create a variable with an empty string to store the results
 var string = "";
 
+// a for loop is needed to cycle through seven times.
+// variable string is used to store and get the # to show up after each cycle.
 for(var i = 1; i < 8; i++){
-    
+    string += "#";
+    console.log(string);
 }
