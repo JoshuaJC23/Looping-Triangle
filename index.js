2f2efb7251d2820cfac76f4735b7d54916d15d26
Ball Man <joshua.christensen13@gmail.com>
2022-01-14 13:07:39 -0700

    added some comments and also changed the one of the strings

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// created a div to grab the element and added some text
 var div = document.getElementById("div");
+
 div.textContent = "Hello Everyone";
 
+// created a new element and attached more text to the main empty element
 var newDiv = document.createElement("p");
-newDiv.textContent = "My name is Thanos"
+newDiv.textContent = "My name is Josh here is the triangle loop exercise"
 
 div.appendChild(newDiv);
 
@@ -11,6 +14,8 @@ var string = "";
 
 // a for loop is needed to cycle through seven times.
 // variable string is used to store and get the # to show up after each cycle.
+// created a new element and attached the new element to the original empty element
+// to display it in the DOM
 for(var i = 1; i < 8; i++){
     string += "#";
     console.log(string);
